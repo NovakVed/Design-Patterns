54e662d55dff9a933348fdbbdb4f3d062739e015
Vedran Novak <9297079+NovakVed@users.noreply.github.com>
2022-01-01 16:36:27 +0100

    LoadPlayer data check

--- src/vednovak_zadaca_1/load/LoadPlayer.java
+++ src/vednovak_zadaca_1/load/LoadPlayer.java
@@ -16,10 +16,34 @@ class LoadPlayer extends LoadData {
         }
     }
 
-    //TODO ubaci provjere igraca
     boolean checkObjectData(String[] object) {
+        return checkIfPlayerHasAllInfo(object);
+    }
+
+    boolean checkIfPlayerHasAllInfo(String[] object) {
         if (object.length != 4) {
-            System.out.printf("ERROR: igrač %10s nema podataka%n", object[1]);
+            if (object.length == 3
+                    && (!object[0].isBlank() || !object[1].isBlank() || !object[2].isBlank())) {
+                System.out.printf("ERROR: igrač %10s nema datum rođenja%n", object[1]);
+            } else {
+                System.out.printf("ERROR: igrač nema sve podatake%n");
+            }
+            return false;
+        }
+        if (object[0].isBlank() || object[0].isEmpty()) {
+            System.out.printf("ERROR: igrač %10s nema klub%n", object[1]);
+            return false;
+        }
+        if (object[1].isBlank() || object[1].isEmpty()) {
+            System.out.printf("ERROR: igrač nema imena%n");
+            return false;
+        }
+        if (object[2].isBlank() || object[2].isEmpty()) {
+            System.out.printf("ERROR: igrač %10s nema poziciju%n", object[1]);
+            return false;
+        }
+        if (object[3].isBlank() || object[3].isEmpty()) {
+            System.out.printf("ERROR: igrač %10s nema datum rođenja%n", object[1]);
             return false;
         }
         return true;
